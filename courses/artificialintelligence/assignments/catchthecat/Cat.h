71655e90ae5d930b1a2eee1bf9c94093d9c3ea27
Liam Harrison <liam.harrison@mymail.champlain.edu>
2023-10-10 20:50:37 -0400

    Updated Cat.h
    Reformatted it to be the correct way to do the algorithm.

--- courses/artificialintelligence/assignments/catchthecat/Cat.h
+++ courses/artificialintelligence/assignments/catchthecat/Cat.h
@@ -2,55 +2,70 @@
 #define CAT_h
 #include "IAgent.h"
 #include <queue>
+#include <map>
 
 struct Cat : public IAgent {
   std::pair<int,int> move(const std::vector<bool>& world, std::pair<int,int> catPos, int sideSize ) override{
 
     //Convert the 1d array to 2d using: [width * row + column]
-    //Convert the 2d array to 1d using: [(i / w) ] [(i % w)]
+    //Convert the 2d array to 1d using: [(i / w)] [(i % w)]
 
-    std::vector<std::pair<int, int>> neighbors = getNeighbors(catPos);
+    std::pair<int, int> exitPoint;
 
-    //TODO: Change this to hold a struct that stores the location and previous instead
-    //first int is location, second is where it came from
     std::queue<std::pair<int, int>> frontier;
 
     //Stores the neighbors that have been reached
     std::vector<int> reached;
 
+    //Map that stores where a point came from. The key is a linearized point, and the data is a 2d point which is where it came from.
+    std::map<int, std::pair<int, int>> cameFrom;
+
     int oneDimensionCatPosition = sideSize * catPos.second + catPos.first;
 
-    //finds the nearest exit:
-    std::pair<int, int> exit = findNearestExit(catPos, world, sideSize);
+    frontier.push(catPos);
 
-    int i;
-    for(i = 0; i < neighbors.size(); i++)
+    //loop to create the cameFrom map
+    while(!frontier.empty()) //loops until the frontier is empty
     {
-      //check if neighbor is false
-      int oneDimensionNeighbor = sideSize * neighbors[i].second + neighbors[i].first;
-      if(!world[oneDimensionNeighbor]) //checks if neighbor is available to move to
+      std::pair currentPos = frontier.front();
+      frontier.pop();
+
+      if(sideSize *  currentPos.second + currentPos.first == sideSize / 2 || sideSize * currentPos.second + currentPos.first == -sideSize / 2)
       {
-        //TODO: check if neighbor has not been reached
-        //TODO: check if it has reached the exit. If it has, exit out of the for loop
-        frontier.emplace(oneDimensionNeighbor, oneDimensionCatPosition); //adds the neighbor and where it came from to the frontier
-        reached.push_back(oneDimensionNeighbor); //adds the neighbor to reached
+        exitPoint = currentPos;
+        break;
       }
+
+      //check neighbors
+      std::vector<std::pair<int, int>> neighbors = getNeighbors(currentPos);
+      int i;
+      for(i = 0; i < neighbors.size(); i++)
+      {
+        //check if neighbor is false
+        int oneDimensionNeighbor = sideSize * neighbors[i].second + neighbors[i].first;
+        if(!world[oneDimensionNeighbor]) //checks if neighbor is available to move to
+        {
+          //TODO: check if neighbor has not been reached
+
+          frontier.emplace(oneDimensionNeighbor, oneDimensionCatPosition); //adds the neighbor and where it came from to the frontier
+          reached.push_back(oneDimensionNeighbor); //adds the neighbor to reached
+          cameFrom.emplace(oneDimensionNeighbor, currentPos); //adds where it came from
+        }
+      }
+
     }
 
-    std::pair<int, int> current = exit;
-    std::pair<int, int> previous;
+    std::pair<int, int> current = exitPoint;
+    std::vector<std::pair<int, int>> path;
 
     //goes through the frontier until it reaches the cat position
     while(current != catPos)
     {
-      previous = current;
-      std::pair<int, int> next =  frontier.front(); //TODO: will have to change this
-      frontier.pop();
-      //sets current to the node that it came from
-      current = std::pair<int, int>(next.second / sideSize, next.second % sideSize);
+      current = cameFrom[sideSize * current.first + current.second];
+      path.push_back(current);
     }
-    return previous; //returns the previous position which is the next for the cat to move
 
+    return path[path.size() -2];
   }
 
   std::vector<std::pair<int, int>> getNeighbors(std::pair<int, int> catP)
@@ -78,21 +93,6 @@ struct Cat : public IAgent {
     }
 
     return neighbors;
-  }
-
-  std::pair<int, int> findNearestExit(std::pair<int, int> catP, const std::vector<bool>& world, int sideSize)
-  {
-    //TODO: find the nearest exit to the cat
-
-    //get all outside hexagons
-
-
-
-    //check which one is the closest to the cat
-    //return that one
-
-
-    return std::pair(0,0); //TODO: change this
   }
 
 };
