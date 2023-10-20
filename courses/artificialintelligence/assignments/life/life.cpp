ad46270c6275d00c86f8f97b62f9349b8181f4af
Liam Harrison <liam.harrison@mymail.champlain.edu>
2023-10-19 20:39:46 -0400

    Worked one life.cpp
    Changed how I was doing a lot of the program. I removed and added some new functions.

--- courses/artificialintelligence/assignments/life/life.cpp
+++ courses/artificialintelligence/assignments/life/life.cpp
@@ -1,153 +1,126 @@
 #include <vector>
 #include <iostream>
-#include <fstream>
+
+//Convert the 1d array to 2d using: [width * row + column]
+//Convert the 2d array to 1d using: [(i / w)] [(i % w)]
 
 //Functions
 
-void getGridSize(std::vector<bool> *grid);
+std::vector<bool> readBoard(const int &columns, const int &rows);
 
-void setGridSize(std::vector<bool> *grid, int x, int y);
+void printBoard(const std::vector<bool> &grid, const int &columns, const int &rows);
 
-std::string getTestFile(std::ifstream *testFile);
+void runRules(std::vector<bool> &grid, const int &columns, const int &rows);
 
-std::ifstream loadTestFile(std::vector<bool> *grid);
-
-void printBoard(std::vector<bool> *grid);
+int numNeighbors(const int &cell, const std::vector<bool> &grid, const int &columns, const int &rows);
 
 int main(){
 
-  std::vector<bool> grid[2];
-  std::fstream file;
+  std::vector<bool> grid;
 
   //clear the board to start
-  grid[0].clear();
-  grid[1].clear();
+  grid.clear();
 
-  //get the grid size from the user
-  getGridSize(grid);
+  int columns, rows, turns;
 
-  //Get the test file from the user
-  //std::string fileName = getTestFile(file);
+  //Takes in input from tests
+  std::cin >> columns >> rows >> turns;
+  grid = readBoard(columns, rows); //creates the board from the test file
 
-  //Load in the test file into the grid
-  loadTestFile(grid);
+  int i;
+  for(i = 0; i < turns; i++) //loops through the specified amount of turns
+  {
+    runRules(grid, columns, rows);
 
-  //prints the board
-  printBoard(grid);
-
-  //TODO: Run all four rules on the board
-
-  //Prints the updated board
-  printBoard(grid);
-
-  //TODO: Compare with output file
+    //Prints the updated board
+    printBoard(grid, columns, rows);
+  }
 
   return 0;
 };
 
-//The purpose of this function is to get the size of the grid from the user
-void getGridSize(std::vector<bool> *grid)
-{
-  int x = 0;
-  int y = 0;
+// The purpose of this function is to create a board based off the input from the test files.
+std::vector<bool> readBoard(int columns, int rows) {
+  std::vector<bool> board;
+  board.reserve(columns * rows);
 
-  std::cout << "How big do you want the board to be?" << std::endl;
-
-  while (x <= 1)
+  char c;  // used to temporarily store the characters coming in from the tests
+  int i, j;
+  for (i = 0; i < columns * rows; i++)
   {
-    std::cout << "Size of x: ";
-    std::cin >> x;
-
-    if(x <= 1)
+    std::cin >> c;
+    switch (c)
     {
-      std::cout << std::endl << "Please type in a number above 0" << std::endl;
+      case '.':
+        board.push_back(false); //pushes an empty space (life does not exist there)
+        break;
+      case '#':
+        board.push_back(true); //pushes a filled space (life exists there)
+        break;
+      default:
+        i--; // go back one
+        break;
     }
   }
+  return board;
+}
 
-  while (y <= 1)
+// The purpose of this function is to print out the entire board
+void printBoard(const std::vector<bool> &grid, int columns, int rows)
+{
+  int x, y;
+  for(x = 0; x < columns; x++)
   {
-    std::cout << std::endl << "Size of y: ";
-    std::cin >> y;
-
-    if(y <= 1)
+    for(y = 0; y < rows; y++)
     {
-      std::cout << std::endl << "Please type in a number above 0" << std::endl;
+      std::cout << (grid[x * (columns * rows) + y] ? '#' : '.');
     }
+    std::cout << std::endl;
   }
-
-  setGridSize(grid, x, y);
 }
 
-//The purpose of this function is to set a new size to the grid
-void setGridSize(std::vector<bool> *grid, int x, int y)
+// The purpose of this function is to run through the grid and perform the rules of the game of life
+void runRules(std::vector<bool> &grid, int columns, int rows)
 {
-  //Resizes the grid to the new size
-  grid[0].resize(x);
-  grid[1].resize(y);
-}
+  /*
+   * Rules:
+  Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+  Any live cell with two or three live neighbours lives on to the next generation.
+  Any live cell with more than three live neighbours dies, as if by overpopulation.
+  Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+   */
 
-//The purpose of this function is to get a file name for a test file from the user
-std::string getTestFile(std::ifstream *file)
-{
-  std::string testFile;
-
-  //Gets a test from user
-  bool validInput = false;
-  while (!validInput)
+  //goes through each cell in the grid, and check the rules against it
+  int x, y, neighbors;
+  for(x = 0; x < columns; x++)
   {
-    std::cout << "Would you like to load in a test?" << std::endl;
-
-    char userInput = ' ';
-    std::cout <<  "(Y/n): ";
-    std::cin >> userInput;
-
-    if(userInput == 'y' || userInput == 'Y')
+    for(y = 0; y < rows; y++)
     {
-      validInput = false;
-      while(!validInput)
-      {
+        neighbors = numNeighbors(columns*rows * x + y, grid, columns, rows); //gets the amount of neighbors for the current cell
 
-        std::string fileName;
-
-        std::cout << "What is the name of the test file you wish to test?" << std::endl;
-        std::cout << "FileName: ";
-        std::cin >> fileName;
-
-        file->open(testFile);
-        if(file->good())
+        if(grid[columns*rows * x + y]) //checks if the cell is alive
         {
-          validInput = true;
-          return fileName;
+          switch (neighbors)
+          {
+            case 2: //nothing needed here because 2 and 3 lead to the same outcome
+            case 3: grid[columns*rows * x + y] = true;
+              break;
+            default: grid[columns*rows * x + y] = false; //any neighbor amount that is not 2 or 3 kills the cell
+              break;
+          }
         }
-        else
+        else if(neighbors == 3) //checks if the dead cell has exactly 3 live neighbors
         {
-          std::cout << "Please input a valid test file" << std::endl;
+          grid[columns*rows * x + y] = true;
         }
-      }
-
-      break;
-    }
-    else if (userInput == 'n' || userInput == 'N')
-    {
-      validInput = true;
-    }
-    else
-    {
-      std::cout << std::endl << "Please type in a valid input. (Y/n)" << std::endl;
     }
   }
 }
 
-//The purpose of this function is to load in a test file into the grid
-std::ifstream loadTestFile(std::vector<bool> *grid)
+//The purpose of this function is to check how many neighbors of a cell are alive
+int numNeighbors(int cell, const std::vector<bool> &grid, int columns, int rows)
 {
-  //TODO: Load in the test file to the board
+  //TODO: check the amount of neighbors the current cell has and return it
+
+  return 0; //TODO: change this
 }
-
-void printBoard(std::vector<bool> *grid)
-{
-  //TODO: Print the board
-}
-
-
-
